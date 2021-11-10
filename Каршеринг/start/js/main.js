71dc116e44ffb31c7ebf8048b449ba9237cb776f
Rustamov Rovwan <rus2691@yandex.ru>
2021-11-10 21:40:23 +0300

    time 3.00

--- Каршеринг/start/js/main.js
+++ Каршеринг/start/js/main.js
@@ -5,4 +5,10 @@ $(function(){
         autoplay: true,
         fade: true,
     });
+    $('.reviews__slaider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1
+    })
 });
