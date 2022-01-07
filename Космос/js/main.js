9e887036935cc9cc1f25d4de17de949cc03562a1
Rustamov Rovwan <rus2691@yandex.ru>
2022-01-07 23:07:40 +0300

    time-01.02

--- Космос/js/main.js
+++ Космос/js/main.js
@@ -1,3 +1,6 @@
 $(function(){
-
+    $('.slider__box').slick({
+        prevArrow: '<img src="images/Arrow-r.svg" alt="" class="slider__arrow slider__arrow-right">', 
+        nextArrow: '<img src="images/Arrow-left.svg" alt="" class="slider__arrow slider__arrow-left">',
+    });
 });
